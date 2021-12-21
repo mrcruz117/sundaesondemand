79c54d2c9a2d237b9c7d6bf1966cbc0792340f04
mrcruz117 <mrcruz1989@gmail.com>
2021-12-21 10:31:15 -0500

    downloaded fixed code

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -5,49 +5,44 @@ import Popover from 'react-bootstrap/Popover';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 
 export default function SummaryForm({ setOrderPhase }) {
-    const [tcChecked, setTcChecked] = useState(false);
+  const [tcChecked, setTcChecked] = useState(false);
 
-    function handleSubmit(event) {
-        event.preventDefault();
+  function handleSubmit(event) {
+    event.preventDefault();
 
-        // pass along to the next phase.
-        // The next page will handle submitting order from context.
-        setOrderPhase('completed');
-    }
+    // pass along to the next phase.
+    // The next page will handle submitting order from context.
+    setOrderPhase('completed');
+  }
 
-    const popover = (
-        <Popover
-            id="termsandconditions-popover"
-            placement="right"
-            positionLeft={200}
-            positionTop={50}
-        >
-            And here's some <strong>amazing</strong> content. It's very engaging. right?
-        </Popover>
-    );
+  const popover = (
+    <Popover id="termsandconditions-popover">
+      <Popover.Content>No ice cream will actually be delivered</Popover.Content>
+    </Popover>
+  );
 
-    const checkboxLabel = (
-        <span>
-            I agree to
-            <OverlayTrigger placement="right" overlay={popover}>
-                <span style={{ color: 'blue' }}> Terms and Conditions</span>
-            </OverlayTrigger>
-        </span>
-    );
+  const checkboxLabel = (
+    <span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      </OverlayTrigger>
+    </span>
+  );
 
-    return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group controlId="terms-and-conditions">
-                <Form.Check
-                    type="checkbox"
-                    checked={tcChecked}
-                    onChange={(e) => setTcChecked(e.target.checked)}
-                    label={checkboxLabel}
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit" disabled={!tcChecked}>
-                Confirm order
-            </Button>
-        </Form>
-    );
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="terms-and-conditions">
+        <Form.Check
+          type="checkbox"
+          checked={tcChecked}
+          onChange={(e) => setTcChecked(e.target.checked)}
+          label={checkboxLabel}
+        />
+      </Form.Group>
+      <Button variant="primary" type="submit" disabled={!tcChecked}>
+        Confirm order
+      </Button>
+    </Form>
+  );
 }
